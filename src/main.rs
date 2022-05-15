f1614d8c0380eabbf87bc2ce4fdca2ad932aecb8
Josef Vacek <vacekj@outlook.com>
2022-05-15 21:37:19 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+	// Two secret prime numbers
+    let p = 53;
+	let q = 59;
+
+	// Small prime number that satisfies:
+	// - Not an integer
+	// - not a factor of n
+	// - 1 < e < Phi(n)
+	// Totient Phi(n) = (p-1)(q-1)
+	let tot = (p-1)*(q-1);
+	let e = 3;
+	let n = p * q;
+	// Public key is n and e
+
+
+	// Generate private key
+	// compute d to satisfy the congruence relation
+	// calculate d=(1+x * tot)/e to be an integer
+	// private key is d, p and q
+
+
+	// encryption
+	// turn plaintext into a number m smaller than n, by using an agreed upon padding scheme.
+	// ciphertext is 
+	// let c = m ^ e mod n;
+
+
+	// decryption
+}
---
+++ target/debug/deps/my_project-97f11c63041cf46e.d
@@ -0,0 +1,5 @@
+/home/runner/HollowIdealisticFirm/target/debug/deps/my_project-97f11c63041cf46e.rmeta: src/main.rs
+
+/home/runner/HollowIdealisticFirm/target/debug/deps/my_project-97f11c63041cf46e.d: src/main.rs
+
+src/main.rs:
